01154e6aac67f021b0b4dd9129be5ec40d76223f
¡Duque <atanaelleonardo@gmail.com>
2024-04-21 00:32:22 -0300

    Add Passenger model to schema
    The diff introduces a new model `Passenger` to the Prisma schema with the following fields: `id`, `name`, and `email`. The `id` field is of type `String

---
+++ prisma/migrations/20240421033207_create_table_passengers/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "passengers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "passengers_pkey" PRIMARY KEY ("id")
+);
